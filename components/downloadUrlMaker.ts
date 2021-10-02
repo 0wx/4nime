7dfacd93a2389ba8330505e2afc3d0760feb3eba
awx <callmeoniichandesu@gmail.com>
2021-10-02 09:52:52 +0700

    App: add `/batch/batchId` endpoint

---
+++ components/downloadUrlMaker.ts
@@ -0,0 +1,41 @@
+export interface Download {
+  host: string
+  url: string
+  videoType: string
+  quality: string
+}
+export const downloadUrlMaker = (download: Download[]) => {
+  const caps: Download[][][] = []
+
+  for (const dl of download) {
+    if (!caps.length) {
+      caps.push([[dl]])
+      continue
+    }
+
+    const indexQuality = caps.findIndex((listByType) => {
+      return listByType.every((list) =>
+        list.every(({ videoType }) => videoType === dl.videoType)
+      )
+    })
+
+    if (indexQuality === -1) {
+      caps.push([[dl]])
+      continue
+    }
+
+    const indexType = caps[indexQuality].findIndex((list) => {
+      return list.every(({ quality }) => quality === dl.quality)
+    })
+
+    if (indexType === -1) {
+      caps[indexQuality].push([dl])
+      continue
+    }
+
+    caps[indexQuality][indexType].push(dl)
+  }
+
+  
+  return caps
+}
---
+++ pages/batch/[batchId].tsx
@@ -0,0 +1,64 @@
+import style from "../../styles/Batch.module.scss"
+import { useEffect, useState } from "react"
+import { useRouter } from "next/router"
+import { Download, downloadUrlMaker } from "../../components/downloadUrlMaker"
+
+interface Batch {
+  title: string
+  thumb: string
+  download: Download[]
+}
+const Batch = () => {
+  const [data, setData] = useState<Batch | 0 | null>(0)
+  const router = useRouter()
+  const batchId = Number(router.query?.batchId)
+
+  useEffect(() => {
+    if (batchId)
+      fetch(`https://same.yui.pw/x/batch/${batchId}`)
+        .then((res) => res.json())
+        .then(setData)
+        .catch((_e) => setData(null))
+    else setData(null)
+  }, [batchId])
+
+  if (typeof data === "number") return <div>Loading...</div>
+  if (!data) return <div>404</div>
+
+  return (
+    <div className={style.container}>
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src={data.thumb} alt={data.title} />
+      <h2 style={{ color: "#eee" }}>{data.title}</h2>
+      <div className={style.url}>
+        {downloadUrlMaker(data.download).map((listByType, index) => {
+          return (
+            <div key={`type${index}`} className={style.type}>
+              <span className={style.typeTitle}>
+                {listByType[0][0].videoType}
+              </span>
+              {listByType.map((listByQuality, index) => {
+                return (
+                  <div key={`quality${index}`} className={style.quality}>
+                    <span className={style.qualityTitle}>
+                      [ {listByQuality[0].quality.trim()} ]
+                    </span>
+                    {listByQuality.map((v, index) => {
+                      return (
+                        <span key={"url" + index} className={style.link}>
+                          <a href={v.url}>{v.host}</a>
+                        </span>
+                      )
+                    })}
+                  </div>
+                )
+              })}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default Batch
